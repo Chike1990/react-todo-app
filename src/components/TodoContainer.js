9c07392d36fa12d59cc8e574540acbbd3cb5e9f6
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-20 19:34:44 +0100

    Setup the files for the todo app

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi Chike</h1>
+        <p>I enjoy learning React</p>
+      </div>
+    )
+  }
+}
+export default TodoContainer;
