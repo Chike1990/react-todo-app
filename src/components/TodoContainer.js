1efade0a6b80eb6fda7ab8a6567367d87e82ddcf
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-21 10:28:24 +0100

    Adds more todo components

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,13 +1,33 @@
+import Header from "./Header";
 import React from "react";
-
+import TodoList from "./TodoList"
 class TodoContainer extends React.Component {
+  state = {
+    todos: [{
+      id: 1,
+      title: "Set up the shared state (props)",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Watch match",
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Sleep",
+      completed: false
+    }
+    ]
+  };
   render() {
     return (
       <div>
-        <h1>Hi Chike</h1>
-        <p>I enjoy learning React</p>
+        <Header />
+        <TodoList todos={this.state.todos} />
       </div>
-    )
+    );
   }
 }
+
 export default TodoContainer;
