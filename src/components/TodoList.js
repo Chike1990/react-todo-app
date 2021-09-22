05149d9075fe1a0862bb935171593400a0b7431d
Chike1990 <chibuikeuzoechina@gmail.com>
2021-09-22 11:53:04 +0100

    Implements the delete button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,7 +5,13 @@ class TodoList extends React.Component{
     return (
       <ul>
         {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo}></TodoItem>
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+          ></TodoItem>
+            // deleteTodoProps={this.props.deleteTodoProps}
         ))}
     </ul>
     )
